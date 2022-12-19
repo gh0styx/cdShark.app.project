06fe35f0a1f73c96062fd064f329d04e76ba5469
gh0sty <nice140@icloud.com>
2022-12-20 00:55:52 +0200

    commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+	distance: '65px',
+	duration: 2600,
+	delay: 450,
+	reset: true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
+sr.reveal('.scroll-down',{delay:500, origin:'right'});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
